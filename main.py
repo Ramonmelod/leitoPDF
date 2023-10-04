ad56a4c73ffb0ed7b546df24f085581bd2357562
Ramon Melo <ramonmelo.com@gmail.com>
2023-10-04 17:21:45 -0300

    implementação da leitura do numero de páginas do pdf

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ import PyPDF2
 pdfArquivo = open(r'C:\Users\ramon\OneDrive\Documentos\GitHub\leitoPDF\RelatóriosUFSCmessetembro.pdf','rb')
 #dadosPDF recebe os dados em binário do
 dadosPDF = PyPDF2.PdfReader(pdfArquivo)
+
+numeroPaginas = len(dadosPDF.pages)
+print(numeroPaginas)
+pdfArquivo.close()
