ae0b0a997579bc76a1ead8db9e1f2cf711e07c17
Ramon Melo <ramonmelo.com@gmail.com>
2023-10-04 17:12:06 -0300

    código até leitura do binário do pdf

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import pandas as pd
-import PyPDF2 as pdf2
+import PyPDF2 
 
-
+# abrindo arquivo em modo leitura e lendo o binário
+pdfArquivo = open(r'C:\Users\ramon\OneDrive\Documentos\GitHub\leitoPDF\RelatóriosUFSCmessetembro.pdf','rb')
+#dadosPDF recebe os dados em binário do
+dadosPDF = PyPDF2.PdfReader(pdfArquivo)
