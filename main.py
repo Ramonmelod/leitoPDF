8f2f7a6dc9b90dfe595c59b820a4f0593f8f8082
Ramon Melo <ramonmelo.com@gmail.com>
2023-10-06 12:16:58 -0300

    implementação do for de leitura de paginas e do write o excel

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import PyPDF2 
+import re
+from openpyxl import load_workbook
 
 # abrindo arquivo em modo leitura e lendo o binário
 pdfArquivo = open(r'C:\Users\ramon\OneDrive\Documentos\GitHub\leitoPDF\RelatóriosUFSCmessetembro.pdf','rb')
@@ -9,11 +11,32 @@ dadosPDF = PyPDF2.PdfReader(pdfArquivo)
 numeroPaginas = len(dadosPDF.pages)
 print('O relatório enviado possui: ' + str(numeroPaginas) + ' páginas\n')
 
-numeroPagina = 0
-paginaLida = dadosPDF.pages[numeroPagina]
+for pagina in range(0, 59): 
+    numeroPagina = pagina                                   #define o numero da pagina a ser lida
+    paginaLida = dadosPDF.pages[pagina]                     #armazena a pagina da itereção em paginaLida no formato
+    textoPagina = paginaLida.extract_text()                 #extrai todo o texto da pagina
+    padrao_data = r'\d{2}/\d{2}/\d{4}'                      #
+    resultado = re.search(padrao_data, textoPagina)
+    data = resultado.group(0)                               # retorna apenas a data
+    print(data + "\n")
 
-textoPagina = paginaLida.extract_text()
+
+#print(textoPagina)
 
 
-print(textoPagina)
+
+
+
+
+
+#print(resultado)
+
 pdfArquivo.close()
+
+wb = load_workbook('controleManutencao.xlsx')
+wb.active
+
+abaControle = wb['controle']
+
+abaControle.cell(row=4, column=4,value=str(data))
+wb.save('controleManutencao.xlsx')
